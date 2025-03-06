9c89c48424b13d7e56fc99467c0379d19e7a5954
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-03-06 18:44:32 +0100

    1

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const userName = document.querySelector("#name-input");
+const userNameSpan = document.querySelector("#name-output");
+
+
+userName.addEventListener('input', function(){
+const name = userName.value.trim();
+userNameSpan.textContent = name === "" ? "Anonymous" : name;
+})
